e6a24676788f8acf196c1c57102e696e9856751b
VladislavZmeev <vladislavzmeev@vk.com>
2026-03-12 22:31:29 +0300

    start of main

--- task_1.2.c
+++ task_1.2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define N 1000
 
 void ADD(int * a, int * b)
 {
@@ -18,6 +19,12 @@ void MOV(int * a, int b)
 
 int main() 
 {
-    
+    int registers[4] = {0, 0, 0, 0};
+    int program[N];
+    int num = 0;
+    for (int i = 0; scanf("%d", &num); i++) {
+        program[i] = num;
+    }
+
     return 0;
 }
