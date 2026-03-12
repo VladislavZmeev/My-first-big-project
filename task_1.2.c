9b730f0f5c52b2dc9146468df39145541417c0ae
VladislavZmeev <vladislavzmeev@vk.com>
2026-03-12 22:08:07 +0300

    ADD, SUB, MOV

---
+++ task_1.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void ADD(int * a, int * b)
+{
+    *a += *b;
+    return;
+}
+void SUB(int * a, int * b)
+{
+    *a -= *b;
+    return;
+}
+void MOV(int * a, int b)
+{
+    *a = b;
+    return;
+}
+
+int main() 
+{
+    
+    return 0;
+}
