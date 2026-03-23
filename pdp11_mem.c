e032be5a609d67c34d5fc41073e01c387c7032a2
VladislavZmeev <vladislavzmeev@vk.com>
2026-03-23 16:39:25 +0300

    w/w

--- pdp11_mem.c
+++ pdp11_mem.c
@@ -14,15 +14,16 @@ byte b_read(Adress adr);
 void w_write(Adress adr, word w);
 word w_read(Adress adr);
 
-void test_mem() 
+void test_mem()  
 {
-    printf("______________________________");
+    printf("==============================\n");
     byte b0 = 0x0a;
     // пишем байт, читаем байт
     b_write(2, b0);
     byte bres = b_read(2);
     printf("bres = %02hhx, b0 = %02hhx\n", bres, b0);
     assert(bres == b0);
+    printf("______________________________\n");
 
     // пишем 2 байта, читаем слово 
     Adress a = 4;
@@ -32,9 +33,16 @@ void test_mem()
     b_write(a, b0);
     b_write(a + 1, b1);
     word wres = w_read(a);
-    printf("wres = %04hhx\nb0 = %02hhx b1 = %02hhx\n");
+    printf("wres = %04hx\nb0 = %02hhx b1 = %02hhx\n", wres, b0, b1);
     assert(wres == w);
-    printf("______________________________");
+    printf("______________________________\n");
+    
+    // пишем слово, читаем 2 байта 
+    a = 8;
+    w = 0x0b0a;
+    w_write(a, w);
+    printf("w = %04hx\nb0 = %02hhx b1 = %02hhx\n", w, mem[a], mem[a+1]);
+    printf("==============================\n");
 }
 int main() 
 {
@@ -53,12 +61,12 @@ byte b_read(Adress adr)
 
 void w_write(Adress adr, word w)
 {
-    mem[adr] = w;
-    mem[adr + 1] = (w >> 8);
+    mem[adr] = w & 0xFF;
+    mem[adr + 1] = (w >> 8) & 0xFF;
 }
 word w_read(Adress adr)
 {
     word w = ((word)(mem[adr + 1])) << 8;
-    w = w | mem[adr] & 0xFF;
+    w = w | (mem[adr] & 0xFF);
     return w;
 }
