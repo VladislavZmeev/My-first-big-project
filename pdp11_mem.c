a808cda49b0b244384a42da30445413aa7c77715
VladislavZmeev <vladislavzmeev@vk.com>
2026-04-05 23:35:13 +0300

    load_data() и mem_dump()

--- pdp11_mem.c
+++ pdp11_mem.c
@@ -8,11 +8,21 @@ typedef word address;              //16 bit
 
 #define MEMESIZE (64*1024) // 64 KB
 byte mem[MEMESIZE]; // память для нашей виртуальной машины
+unsigned long long int coounter;
 
 void b_write(address adr, byte b);
 byte b_read(address adr);
 void w_write(address adr, word w);
 word w_read(address adr);
+
+void load_data();
+// Все числа шестнадцатеричные.
+// Файл состоит из 1 или более блоков.
+// Блок начинается со строки где через пробел записаны адрес начала блока и N сколько байт в этом блоке.
+// далее идут N строк, по 1 байту на строку.
+void mem_dump(address adr, int size);
+// печатает size байт, начиная с адреса adr в виде слов по формату "%06o: %06o %04x":
+// адрес: восьмеричное_слово шестнадцатеричное_слово
 
 void test1_mem(address adr, word w, byte expb1, byte expb0)  
 {
@@ -96,4 +106,40 @@ word w_read(address adr)
     word w = ((word)(mem[adr + 1])) << 8;
     w = w | (mem[adr] & 0xFF);
     return w;
+}
+
+void load_data()
+{
+    address adr;
+    unsigned long int n;
+    byte b;
+    
+    while(scanf("%hx %lx", &adr, &n) == 2)
+    {
+        for(unsigned long int i = 0; i < n; i++)
+        {
+            scanf("%hhx", &b);
+            if(adr + i < MEMESIZE)  // проверка границ
+                mem[adr + i] = b;
+        }
+    }
+}
+
+void mem_dump(address adr, int size)
+{
+    // Печатаем побайтово, но в выводе группируем по словам (2 байта)
+    for(int i = 0; i < size; i += 2)
+    {
+        if(i + 1 < size)
+        {
+            word w = b_read(adr + i) | (b_read(adr + i + 1) << 8);
+            printf("%06o: %06o %04x\n", adr + i, w, w);
+        }
+        else
+        {
+            // Если остался один байт (нечетное количество)
+            word w = b_read(adr + i);
+            printf("%06o: %06o %04x\n", adr + i, w, w);
+        }
+    }
 }
